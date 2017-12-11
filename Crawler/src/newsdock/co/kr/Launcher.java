e0ab606d68fbc0440b8bdfbb5da36556686620a0
TeRua <kwakms05@gmail.com>
2017-12-11 15:21:59 +0900

    Checkpoints. Changing role of Crawler into Thread. and move its roles to Launcher class.

--- Crawler/src/newsdock/co/kr/Launcher.java
+++ Crawler/src/newsdock/co/kr/Launcher.java
@@ -1,14 +1,41 @@
 package newsdock.co.kr;
 
-public class Launcher {
-	private Crawler crawler;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import newsdock.co.kr.Connector.DBConnector;
+import newsdock.co.kr.DO.Feed;
+import newsdock.co.kr.DO.Tag;
+
+public class Launcher {
+	// private Crawler crawler;
+
+	private ArrayList<Feed> feeds;
+	private ArrayList<Tag> tags;
+	private DBConnector dbConnector;
+	private Connection conn = null;
+	
 	public void init() {
-		crawler = new Crawler();
-		crawler.initCrawler();
+		// Get DB Connection.
+		try {
+			if (conn.isClosed() || conn.equals(null)) {
+				// Check whether connection is closed or created.
+				dbConnector = new DBConnector();
+				conn = dbConnector.initConnector();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Acquire Feeds.
+		feeds = dbConnector.loadFeeds();
+		// Acquire Tag Informations.
+		tags = dbConnector.loadTags();
+
 	}
 
 	public void startCrawling() {
-		crawler.startCrawl();
+
 	}
 }
